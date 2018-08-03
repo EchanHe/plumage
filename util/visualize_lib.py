9b1ceb03def7f06ad3bf52b7dead6b41117f6ff5
echanhe <csyichenhe@gmail.com>
2018-08-03 15:05:58 +0100

    update the visualize libs, Can plot coordinates and the coordinates of the patch polygons and outline polygon.

--- util/visualize_lib.py
+++ util/visualize_lib.py
@@ -37,15 +37,15 @@ def save_masks_on_image(images, pred_segs, save_path, fig_names=None):
     """ 
     _check_shape_NHW(images ,pred_segs)  
     # Set the figure params
-    if images.shape[0] < 10:
-        batch = images.shape[0]
+    result_size = images.shape[0]
+    if result_size < 10:
+        batch = result_size
     else:
         batch = 10
     nrows = 2
     ncols =5
     alpha = 0.3
     
-    result_size = pred_segs.shape[0]
     
     cl, n_cl = extract_classes(pred_segs)
     
@@ -86,15 +86,16 @@ def save_pred_diff_on_image(images, gt_segs, pred_segs , c , save_path , fig_nam
     _check_shape_NHW(images ,pred_segs, gt_segs)
 
     # Set the figure params
-    if images.shape[0] < 10:
-        batch = gt_segs.shape[0]
+    result_size = gt_segs.shape[0]
+    if result_size < 10:
+        batch = result_size
     else:
         batch = 10
     nrows = 2
     ncols =5
     alpha = 0.3
     
-    result_size = gt_segs.shape[0]
+    
     
     gt_mask = gt_segs == c
     pred_mask = pred_segs == c
@@ -142,14 +143,15 @@ def save_masks(gt_segs, pred_segs, save_path, fig_names=None):
     _check_shape_NHW(pred_segs, gt_segs)
 
     # Set the figure params
-    if gt_segs.shape[0] < 10:
-        batch = gt_segs.shape[0]
+    result_size = gt_segs.shape[0]
+    if result_size < 10:
+        batch = result_size
     else:
         batch = 10
     nrows = 10
     ncols =3
     
-    result_size = gt_segs.shape[0]
+    
     intersection = (gt_segs == pred_segs)
     plot_list = [pred_segs,gt_segs,intersection]
     
@@ -172,13 +174,144 @@ def save_masks(gt_segs, pred_segs, save_path, fig_names=None):
 
 #### Coords part ######
 
+LM_CNT = 5
+
+def show_markups(imgs , pred_coords = None , pred_patches =None , pred_contours=None,
+    gt_coords =None , gt_patches=None, gt_contours=None, save_path = None , fig_names = None):
+    """
+    Plot the markups and images
+    
+    params:
+        img: [batch, height, width, 3]
+        gt / pred_coords: coordinations [batch , landmark *2 ]    
+        gt / pred_patches: a list of patches coords  [batch][patches count][patch points *2 ].
+        gt / pred_contours: a list of contours coords  [batch][contours count][contour points *2 ].
+        save_path directory of saving figures
+        fig_names: a list of img names that used to name the figure
+    """
+    result_size = imgs.shape[0]
+    if result_size < 10:
+        batch = result_size
+    else:
+        batch = 10
+
+    ncols = batch
+    # batch_size = ncols = imgs.shape[0]
+    # assert batch_size <=20, "The batch size is larger than 20, bad for plotting"
+    nrows = 1
+
+    
+
+    for i_start in range(0,result_size,batch):
+        fig  = plt.figure(figsize=(ncols * 10, nrows *10))
+        for i_row in range(i_start, i_start+batch):
+            plt.subplot(nrows, ncols,(i_row)%(ncols*nrows)+1)
+
+            show_one_markup(plt, img = imgs[i_row,...] ,
+            pred_coord = _none_or_element(pred_coords ,i_row) , pred_patch =_none_or_element(pred_patches ,i_row),
+            pred_contour = _none_or_element(pred_contours ,i_row),
+            gt_coord = _none_or_element(gt_coords ,i_row), gt_patch =   _none_or_element(gt_patches ,i_row),
+            gt_contour = _none_or_element(gt_contours ,i_row),
+            fig_name = _none_or_element(fig_names ,i_row))
+
+        if save_path is not None:
+            fig.savefig(save_path+"coords_{}.jpg".format(i_start))
+        plt.close(fig)
 
 
+def show_one_markup(plt, img, pred_coord , pred_patch , pred_contour,
+    gt_coord  , gt_patch, gt_contour, fig_name = ""):
+    """
+    Plot one image and markup using show_coords and show_patches
+
+    params:
+        plt : pyplot
+        img: [height, width, 3]
+        gt / pred_coord: coordinations [landmark *2 ]    
+        gt / pred_patch: a list of patches coords [patches count][patch points *2 ].
+        gt / pred_contour: a list of contours coords [contours count][contour points *2 ].
+        fig_name: The img name that used to name current figure
+    """
+
+    plt.title(fig_name , fontsize = 20)
+    plt.imshow(img)
+    # Show the predict mark and pred_patch
+    show_patches(plt, pred_patch)
+    show_patches(plt, pred_contour , is_patch = False)
+    show_coords(plt, pred_coord)
+
+    show_patches(plt, gt_patch, 'red')
+    show_patches(plt, gt_contour , 'red', is_patch = False)
+    show_coords(plt, gt_coord, 'red')
+
+
+
+def show_coords(plt, coord , color = 'cyan'):
+    """
+    Plot the 2D points on figure
+
+    params:
+        plt : pyplot
+        coord [landmark *2 ]. eg [x1,y1, ... , xn, yn]
+        lm_cnt: The landmark count
+        color: The color of the point
+
+    """
+    if coord is None:
+        return 0
+    lm_cnt = LM_CNT
+    cols_num_per_coord = 2
+    # lm_cnt = 5
+
+    for i_col in range(lm_cnt):
+        x = coord[ i_col * cols_num_per_coord]
+        y = coord[ i_col * cols_num_per_coord +1] 
+        if x >= 0 and ~np.isnan(x):
+            plt.plot(x, y, 'x' , alpha=0.8 , mew = 4 , mec = color )
+
+patches_names = [ 'poly.crown', 'poly.nape','poly.mantle', 'poly.rump', 'poly.tail'
+, 'poly.wing.coverts',   'poly.wing.primaries.secondaries',
+ 'poly.throat', 'poly.breast', 'poly.belly', 'poly.tail.underside']
+
+def show_patches(plt,patch, color = 'cyan' , is_patch = True ):
+    """
+    Plot the patches on figure
+
+    params:
+        plt : pyplot
+        patch [patches count][patch points *2 ]. eg [x1,y1, ... , xn, yn]
+    """ 
+    if patch is None:
+        return 0
+
+    for id_p, p_coord in enumerate(patch):
+        # print(p_coord)
+        length = p_coord.shape[0]
+        if length>=2:
+            for i in range(0,length,2):
+                x = (p_coord[i%length] , p_coord[(i+2)%length])
+                y = (p_coord[(i+1)%length], p_coord[(i+3)%length])
+                plt.plot(x,y,color = color,lw =2)
+            if is_patch:
+                plt.text(p_coord[0] , p_coord[1]-20 ,
+                    patches_names[id_p][5:], 
+                    fontdict={'color': color,'size':12 })
+                
+                
 
 #### Coords part ######s
 
 
 ##small util##
+
+def _none_or_element(array , id_batch):
+    if array is not None:
+
+        return array[id_batch]
+    else:
+        return None
+
+
 def _check_shape_NHW(a,b,*args):
     cond = (a.shape[:3] == b.shape[:3])
     for count, thing in enumerate(args): 
