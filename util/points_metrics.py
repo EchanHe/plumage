fe7dd768eaa52bc309425276697c9b7bad3524fa
echanhe <csyichenhe@gmail.com>
2019-02-14 15:22:04 +0000

    Add patch intensity comparsion.

--- util/points_metrics.py
+++ util/points_metrics.py
@@ -1,8 +1,8 @@
 import numpy as np
-
-
-
-
+from points_util import create_rect_on_coords_fixed_length , create_rect_on_coords_proportion_length 
+import cv2
+from scipy.stats import pearsonr
+import collections
 def pck_accuracy(pred_coords, gt_coords , lm_cnt , pck_threshold, scale = 1 ):
     """
     Goal: calculate different metrics between prediction and gt
@@ -64,3 +64,263 @@ def in_area_rate(pred_coords ,gt_coords, masks):
     # print(np.sum(result==True )/result.size)
     # print(np.nanmean(result , axis = 0))
     return np.nanmean(result , axis = 0)
+
+
+def pixel_difference(gt_coords , pred_coords, files_names , img_folder ):
+    # length_choices = np.arange(10,150,50)
+    length_choices = [10,50,100,150,200]
+    all_length_gt_patches = {}
+    all_length_pred_patches = {}
+
+    all_length_gt_pixel = {}
+    all_length_pred_pixel = {}
+
+    pred_coords[gt_coords==-1]=-1
+    for idx,length in enumerate(length_choices):
+        all_length_gt_patches[str(length)] = create_rect_on_coords(gt_coords, length,length)
+        all_length_pred_patches[str(length)] = create_rect_on_coords(pred_coords, length,length)
+        
+        num_of_pats = all_length_pred_patches[str(length)].shape[-1]
+        
+        all_length_gt_pixel[str(length)] = [[]] * num_of_pats
+        all_length_pred_pixel[str(length)] =[[]] * num_of_pats
+
+    final_result = {}    
+
+    for batch_idx in range(gt_coords.shape[0]):
+        # Read img for each file names
+        filename = img_folder + files_names[batch_idx]
+        img = cv2.imread(filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
+        
+        for key in all_length_gt_patches:
+            
+            all_pred_patches = all_length_pred_patches[key]
+            pred_patches = all_pred_patches[batch_idx]
+            for idx, pat in enumerate(pred_patches):
+                if pat !=-1:
+                    rect_coords = eval('[' + pat +']')
+                    x = np.arange(rect_coords[0],rect_coords[1])
+                    y = np.arange(rect_coords[2],rect_coords[3])
+                    pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+                    mean_pixel = list(np.round(np.mean(pixels,axis = 0)))
+                    
+                    all_length_pred_pixel[key][idx] = all_length_pred_pixel[key][idx] + (mean_pixel)
+
+            all_gt_patches = all_length_gt_patches[key]
+            gt_patches = all_gt_patches[batch_idx]
+            for idx, pat in enumerate(gt_patches):
+                if pat !=-1:
+                    rect_coords = eval('[' + pat +']')
+                    x = np.arange(rect_coords[0],rect_coords[1])
+                    y = np.arange(rect_coords[2],rect_coords[3])
+                    pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                    mean_pixel = list(np.round(np.mean(pixels,axis = 0)))
+                    all_length_gt_pixel[key][idx] = all_length_gt_pixel[key][idx] + (mean_pixel)
+            
+            difference = np.array([])
+            for gt,pred in zip(all_length_gt_pixel[key], all_length_pred_pixel[key]):
+    #             print("length of patches: ", key)
+    #             print(len(gt),len(pred))
+    #             print("correlation: ", pearsonr(gt,pred))    
+                result = np.absolute(np.subtract(gt, pred))
+    #             print("pixel difference: ", np.mean(result))
+                difference = np.append(difference, round(np.nanmean(result),1))
+            final_result["bbox_"+key] = difference
+
+    return final_result
+
+
+def pixel_difference_single(gt_coords , pred_coords,length, proportion, files_names , img_folder , height=None ):
+    if height is None:
+        height = length
+
+    result_key = "Width:{}_Height:{}_Proportion:{}".format(length , height , proportion)
+
+    if proportion is None:
+        all_gt_patches = create_rect_on_coords_fixed_length(gt_coords, length,height)
+        all_pred_patches = create_rect_on_coords_fixed_length(pred_coords,length,height)
+    else:
+        all_gt_patches = create_rect_on_coords_proportion_length(gt_coords , width =length, height=height , proportion = proportion)
+        all_pred_patches = create_rect_on_coords_proportion_length(pred_coords , width =length, height=height , proportion = proportion)
+
+    pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    gt_pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    names_list = np.array([])
+    
+    final_result = collections.defaultdict(dict)
+
+    for batch_idx in range(gt_coords.shape[0]):
+        filename = img_folder + files_names[batch_idx]
+        names_list = np.append(names_list,files_names[batch_idx])
+        
+        img = cv2.imread(filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     img = cv2.resize(img, dsize=(width,height), interpolation=cv2.INTER_CUBIC)
+        
+        # get the right patches for the image.
+        pred_patches = all_pred_patches[batch_idx]
+        for idx, pat in enumerate(pred_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1]).astype(int)
+                y = np.arange(rect_coords[2],rect_coords[3]).astype(int)
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                pixel_per_patch[idx] = pixel_per_patch[idx] + (mean_pixel)
+        
+        gt_patches = all_gt_patches[batch_idx]
+        for idx, pat in enumerate(gt_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1]).astype(int)
+                y = np.arange(rect_coords[2],rect_coords[3]).astype(int)
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                gt_pixel_per_patch[idx] = gt_pixel_per_patch[idx] + (mean_pixel)
+    
+    
+    difference = np.array([])
+    coeff_list = np.array([])
+    p_value_list = np.array([])
+
+    for gt,pred in zip(gt_pixel_per_patch, pixel_per_patch):
+        result = np.absolute(np.subtract(gt, pred))
+#             print("pixel difference: ", np.mean(result))
+        coeff, p_value = pearsonr(gt,pred)
+        coeff_list = np.append(coeff_list, coeff)
+        p_value_list = np.append(p_value_list , p_value)
+        difference = np.append(difference, round(np.nanmean(result),1))
+
+
+    final_result[result_key]['diff'] = difference
+    final_result[result_key]['coeff'] = coeff_list
+    final_result[result_key]['p_value'] = p_value_list
+    final_result[result_key]['gt_pixel_patches'] = gt_pixel_per_patch
+    final_result[result_key]['pred_pixel_patches'] = pixel_per_patch
+    final_result[result_key]['file_name'] = names_list
+    return final_result
+
+
+
+
+## deprecated ##
+
+
+def pixel_difference_single_length(gt_coords , pred_coords,length, files_names , img_folder , height=None):
+    if height is None:
+        height = length
+    all_gt_patches = create_rect_on_coords(gt_coords, length,height)
+    all_pred_patches = create_rect_on_coords(pred_coords,length,height)
+
+
+    pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    gt_pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    final_result = collections.defaultdict(dict)
+
+    for batch_idx in range(gt_coords.shape[0]):
+        filename = img_folder + files_names[batch_idx]
+        img = cv2.imread(filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     img = cv2.resize(img, dsize=(width,height), interpolation=cv2.INTER_CUBIC)
+        
+        # get the right patches for the image.
+        pred_patches = all_pred_patches[batch_idx]
+        for idx, pat in enumerate(pred_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1])
+                y = np.arange(rect_coords[2],rect_coords[3])
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                pixel_per_patch[idx] = pixel_per_patch[idx] + (mean_pixel)
+        
+        gt_patches = all_gt_patches[batch_idx]
+        for idx, pat in enumerate(gt_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1])
+                y = np.arange(rect_coords[2],rect_coords[3])
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                gt_pixel_per_patch[idx] = gt_pixel_per_patch[idx] + (mean_pixel)
+        difference = np.array([])
+        coeff_list = np.array([])
+        p_value_list = np.array([])
+        
+        for gt,pred in zip(gt_pixel_per_patch, pixel_per_patch):
+            result = np.absolute(np.subtract(gt, pred))
+#             print("pixel difference: ", np.mean(result))
+            coeff, p_value = pearsonr(gt,pred)
+            coeff_list = np.append(coeff_list, coeff)
+            p_value_list = np.append(p_value_list , p_value)
+            difference = np.append(difference, round(np.nanmean(result),1))
+        final_result[str(length)]['diff'] = difference
+        final_result[str(length)]['coeff'] = coeff_list
+        final_result[str(length)]['p_value'] = p_value_list
+        final_result[str(length)]['gt_pixel_patches'] = gt_pixel_per_patch
+        final_result[str(length)]['pred_pixel_patches'] = pixel_per_patch
+    return final_result
+
+
+def pixel_difference_single_proportion(gt_coords , pred_coords,length, files_names , img_folder , height=None , proportion):
+    if height is None:
+        height = length
+
+    all_gt_patches = create_rect_on_coords_proportion_length(gt_coords , width =length, height=height , proportion = proportion)
+    all_pred_patches = create_rect_on_coords_proportion_length(pred_coords , width =length, height=height , proportion = proportion)
+
+    pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    gt_pixel_per_patch = [[]] * all_pred_patches.shape[-1]
+    final_result = collections.defaultdict(dict)
+
+    for batch_idx in range(gt_coords.shape[0]):
+        filename = img_folder + files_names[batch_idx]
+        img = cv2.imread(filename)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     img = cv2.resize(img, dsize=(width,height), interpolation=cv2.INTER_CUBIC)
+        
+        # get the right patches for the image.
+        pred_patches = all_pred_patches[batch_idx]
+        for idx, pat in enumerate(pred_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1])
+                y = np.arange(rect_coords[2],rect_coords[3])
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                pixel_per_patch[idx] = pixel_per_patch[idx] + (mean_pixel)
+        
+        gt_patches = all_gt_patches[batch_idx]
+        for idx, pat in enumerate(gt_patches):
+            if pat !=-1:
+                rect_coords = eval('[' + pat +']')
+                x = np.arange(rect_coords[0],rect_coords[1])
+                y = np.arange(rect_coords[2],rect_coords[3])
+                pixels = img[np.repeat(y,len(x)) , np.tile(x, len(y)),:]
+
+                mean_pixel = list(np.round(np.nanmean(pixels,axis = 0)))
+                gt_pixel_per_patch[idx] = gt_pixel_per_patch[idx] + (mean_pixel)
+        difference = np.array([])
+        coeff_list = np.array([])
+        p_value_list = np.array([])
+        
+        for gt,pred in zip(gt_pixel_per_patch, pixel_per_patch):
+            result = np.absolute(np.subtract(gt, pred))
+#             print("pixel difference: ", np.mean(result))
+            coeff, p_value = pearsonr(gt,pred)
+            coeff_list = np.append(coeff_list, coeff)
+            p_value_list = np.append(p_value_list , p_value)
+            difference = np.append(difference, round(np.nanmean(result),1))
+        final_result[str(length)]['diff'] = difference
+        final_result[str(length)]['coeff'] = coeff_list
+        final_result[str(length)]['p_value'] = p_value_list
+        final_result[str(length)]['gt_pixel_patches'] = gt_pixel_per_patch
+        final_result[str(length)]['pred_pixel_patches'] = pixel_per_patch
+    return final_result
--- util/points_util.py
+++ util/points_util.py
@@ -30,13 +30,15 @@ def heatmap_to_coord(heatmaps , ori_width , ori_height):
 # Goals: transfer the coords into a rectangle coords
 
 #return a matrix of rectangle x,y strings in [batch, n_patches]
-def pred_coords_to_patches(pred_coords , half_width =10, half_height=10 , ignore_coords =10):
+def pred_coords_to_patches(pred_coords , width =10,height=10 , ignore_coords =10):
 
-    pred_coords = pred_coords[:,ignore_coords:]
-
+    # pred_coords = pred_coords[:,ignore_coords:]
     total_patches = pred_coords.shape[-1]//2
     x_coords = pred_coords[:,range(0,total_patches*2,2)]
     y_coords = pred_coords[:,range(1,total_patches*2,2)]
+
+    half_width = width//2
+    half_height = height //2
 
     upper_x_coords = x_coords + half_width
     upper_y_coords = y_coords + half_height
@@ -56,12 +58,12 @@ def pred_coords_to_patches(pred_coords , half_width =10, half_height=10 , ignore
             l_y = lower_y_coords[i,j]
             patch_coords = [l_x,u_y,u_x,u_y,u_x,l_y,l_x,l_y]
             pred_patches[i, j] = ','.join(str(int(c)) for c in patch_coords )
+    pred_patches[x_coords == -1] =-1
     return pred_patches
 
 
 
-#return a matrix of rectangle x,y strings in [batch, n_patches]
-def create_rect_on_coords(coords , width =10, height=10 , ignore_coords =10):
+def create_rect_on_coords_fixed_length(coords , width =10, height=10 , circular_indexing = False):
     """
     Goal: a N_D array of rectangle using coords as center 
 
@@ -97,7 +99,93 @@ def create_rect_on_coords(coords , width =10, height=10 , ignore_coords =10):
             u_y = upper_y_coords[i,j]
             l_x = lower_x_coords[i,j]
             l_y = lower_y_coords[i,j]
-            patch_coords = [l_x,u_x,l_y,u_y]
-            pred_patches[i, j] = ','.join(str(int(c)) for c in patch_coords )
+            if circular_indexing:
+                patch_coords = [l_x,u_y,u_x,u_y,u_x,l_y,l_x,l_y]
+            else:
+                patch_coords = [l_x,u_x,l_y,u_y]
+            pred_patches[i, j] = ','.join(str(c) for c in patch_coords )
     pred_patches[x_coords == -1] =-1
+    return pred_patches
+
+
+def create_rect_on_coords_proportion_length(coords , width =20, height=20 , proportion = 0.2, ignore_coords = 5 , circular_indexing = False):
+    """
+    Goal: a N_D array of rectangle using coords as center
+        Using the proportion of distance between body region as rectangle length
+
+    params:
+        coords: [batch, x1,y1,..., xn,yn]
+
+    return:
+        x,y strings in [batch, n_patches]
+    """
+
+    # coords = coords[:,ignore_coords:]
+
+    total_patches = coords.shape[-1]//2
+    coords = coords.copy()
+    coords[coords==-1] = np.nan
+    
+    x_coords = coords[:,range(0,total_patches*2,2)]
+    y_coords = coords[:,range(1,total_patches*2,2)]
+
+    half_width = width//2
+    half_height = height //2
+
+    
+    upper_x_coords = x_coords + half_width
+    upper_y_coords = y_coords + half_height
+    lower_x_coords = x_coords - half_width
+    lower_y_coords = y_coords - half_height
+    
+    pred_patches = np.empty((coords.shape[0] , total_patches) , dtype = object)
+    for i in range(coords.shape[0]):
+        for j in range(total_patches):
+            if j > ignore_coords and j < total_patches-1:
+                
+                left_body_part_dist = x_coords[i,j] - x_coords[i,j-1]
+                right_body_part_dist = x_coords[i,j+1] - x_coords[i,j]
+                
+                if not np.isnan(right_body_part_dist) and ((right_body_part_dist * proportion) //2)>1:
+                    u_x = x_coords[i,j] + (right_body_part_dist * proportion) //2
+                else:
+                    u_x = x_coords[i,j] + half_width
+                    
+                if not np.isnan(left_body_part_dist) and ((left_body_part_dist * proportion) //2)>1:
+                    l_x = x_coords[i,j] - (left_body_part_dist * proportion) //2
+                else:
+                    l_x = x_coords[i,j] -half_width
+                    
+            elif j == ignore_coords:
+                right_body_part_dist = x_coords[i,j+1] - x_coords[i,j]
+                if not np.isnan(right_body_part_dist) and ((right_body_part_dist * proportion) //2)>1:
+                    u_x = x_coords[i,j] + (right_body_part_dist * proportion) //2
+                    l_x = x_coords[i,j] - (right_body_part_dist * proportion) //2
+                else:
+                    u_x = x_coords[i,j] + half_width
+                    l_x = x_coords[i,j] -half_width
+                    
+            elif j == total_patches-1:
+                left_body_part_dist = x_coords[i,j] - x_coords[i,j-1]
+                if not np.isnan(left_body_part_dist) and ((left_body_part_dist * proportion) //2)>1:
+                    u_x = x_coords[i,j] + (left_body_part_dist * proportion) //2
+                    l_x = x_coords[i,j] - (left_body_part_dist * proportion) //2
+                else:
+                    u_x = x_coords[i,j] + half_width
+                    l_x = x_coords[i,j] - half_width
+                    
+            else:
+                u_x = upper_x_coords[i,j]
+                l_x = lower_x_coords[i,j]
+            
+            u_y = upper_y_coords[i,j]
+            l_y = lower_y_coords[i,j]
+            
+            if circular_indexing:
+                patch_coords = [l_x,u_y,u_x,u_y,u_x,l_y,l_x,l_y]
+            else:
+                patch_coords = [l_x,u_x,l_y,u_y]
+            pred_patches[i, j] = ','.join(str(c) for c in patch_coords )
+
+    pred_patches[np.isnan(x_coords)] =-1
     return pred_patches
